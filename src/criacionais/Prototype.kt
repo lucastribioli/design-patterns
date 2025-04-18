6b51a26cae5946db09c9236ef851a1a20217fd1f
Lucas Tribioli <lucas.tribioli@clickbus.com>
2025-04-18 18:48:22 -0300

    feat: implementation class Prototype

--- src/criacionais/Prototype.kt
+++ src/criacionais/Prototype.kt
@@ -1,4 +1,19 @@
 package criacionais
 
-class Prototype {
+fun main() {
+    val original = Cartao("João", "1234-5678", "12/25")
+    val clone = original.clone()
+    println("Original: ${original.name} objeto: ${original}")
+    println("Clone: ${clone.name} objeto: ${clone}")
+}
+
+// Os Data Class já tem esse desing pattern implementado com o método copy
+class Cartao(
+    val name: String,
+    val numero: String,
+    val validade: String,
+) {
+    fun clone(): Cartao {
+        return Cartao(name, numero, validade)
+    }
 }

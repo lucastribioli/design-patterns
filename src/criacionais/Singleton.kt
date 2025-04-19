2bf928ec5a9e25d2acbcb01aa6521b8a39e051a1
Lucas Tribioli <lucas.tribioli@clickbus.com>
2025-04-19 16:38:11 -0300

    feat: implementation class Singleton

--- src/criacionais/Singleton.kt
+++ src/criacionais/Singleton.kt
@@ -1,4 +1,18 @@
 package criacionais
 
-class Singleton {
+fun main() {
+    val singleton1 = Singleton.getInstance()
+    val singleton2 = Singleton.getInstance()
+
+    println(singleton1 == singleton2) // true
+}
+object Singleton {
+    private var instance: Singleton? = null
+
+    fun getInstance(): Singleton {
+        if (instance == null) {
+            instance = Singleton
+        }
+        return instance!!
+    }
 }

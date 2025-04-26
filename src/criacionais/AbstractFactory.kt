ee55a224a2f211cfbfe59a0fbfff39b7411c86b4
Lucas Tribioli <lucas.tribioli@clickbus.com>
2025-04-26 15:03:23 -0300

    feat: implements AbstractFactory.kt.kt

--- src/criacionais/AbstractFactory.kt
+++ src/criacionais/AbstractFactory.kt
@@ -1,4 +1,74 @@
 package criacionais
 
-class AbstractFactory {
+
+fun main() {
+    val pedidoSimples = PedidoSimples()
+    val bebidaSimples = pedidoSimples.criarBebida()
+    val comidaSimples = pedidoSimples.criarComida()
+
+    bebidaSimples.beber()
+    comidaSimples.comer()
+
+    val pedidoChique = PedidoChique()
+    val bebidaChique = pedidoChique.criarBebida()
+    val comidaChique = pedidoChique.criarComida()
+
+    bebidaChique.beber()
+    comidaChique.comer()
+
+}
+
+interface Bebida {
+    fun beber()
+}
+
+class Cafe : Bebida {
+    override fun beber() {
+        println("Bebendo café")
+    }
+}
+
+class Cha : Bebida {
+    override fun beber() {
+        println("Bebendo chá")
+    }
+}
+
+interface Comida {
+    fun comer()
+}
+
+class Pao : Comida {
+    override fun comer() {
+        println("Comendo pão")
+    }
+}
+
+class Bolo : Comida {
+    override fun comer() {
+        println("Comendo bolo")
+    }
+}
+
+interface FabricaPedido {
+    fun criarBebida(): Bebida
+    fun criarComida(): Comida
+}
+class PedidoSimples: FabricaPedido {
+    override fun criarBebida(): Bebida {
+        return Cafe()
+    }
+
+    override fun criarComida(): Comida {
+        return Pao()
+    }
+}
+class PedidoChique: FabricaPedido {
+    override fun criarBebida(): Bebida {
+        return Cha()
+    }
+
+    override fun criarComida(): Comida {
+        return Bolo()
+    }
 }

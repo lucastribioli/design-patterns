a3a914a3d7eab2b6ca823fd7a547ed2130d04245
Lucas Tribioli <lucas.tribioli@clickbus.com>
2025-04-26 14:48:33 -0300

    feat: implements FactoryMethod.kt

--- src/criacionais/FactoryMethod.kt
+++ src/criacionais/FactoryMethod.kt
@@ -1,4 +1,50 @@
 package criacionais
 
-class FactoryMethod {
+
+fun main() {
+    val factory = TransporteFactory()
+    val navio = factory.criarTransporte("navio")
+    val carro = factory.criarTransporte("carro")
+
+    navio.carregar()
+    navio.entregar()
+
+    carro.carregar()
+    carro.entregar()
+}
+
+interface Transporte{
+
+    fun entregar()
+    fun carregar()
+}
+
+class Navio : Transporte {
+    override fun entregar() {
+        println("Entregando por navio")
+    }
+
+    override fun carregar() {
+        println("Carregando no navio")
+    }
+}
+
+class Carro : Transporte {
+    override fun entregar() {
+        println("Entregando por carro")
+    }
+
+    override fun carregar() {
+        println("Carregando no carro")
+    }
+}
+
+class TransporteFactory {
+    fun criarTransporte(tipo: String): Transporte {
+        return when (tipo) {
+            "navio" -> Navio()
+            "carro" -> Carro()
+            else -> throw IllegalArgumentException("Tipo de transporte desconhecido")
+        }
+    }
 }

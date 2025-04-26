73f3cc61fec6d05fe4d45e0fe39bd23c90f5f9a8
Lucas Tribioli <lucas.tribioli@clickbus.com>
2025-04-26 12:30:21 -0300

    feat: implements class StaticFactoryMethod

--- src/criacionais/StaticFactoryMethod.kt
+++ src/criacionais/StaticFactoryMethod.kt
@@ -1,4 +1,26 @@
 package criacionais
 
-class StaticFactoryMethod {
+fun main() {
+    Aluno.of(
+        "Lucas",
+        22,
+        "rua tal",
+        "123",
+        "t@t.com"
+    )
+}
+
+class Aluno(
+    val nome: String,
+    val idade: Int,
+    val endereco: String,
+    val telefone: String,
+    val email: String,
+    val escola:String
+) {
+    companion object {
+        fun of(nome: String, idade: Int, endereco: String, telefone: String, email: String): Aluno {
+            return Aluno(nome, idade, endereco, telefone, email, "escola tal")
+        }
+    }
 }

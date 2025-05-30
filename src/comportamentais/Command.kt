a7ce5a545dc9042e2f427cd9f9241433fb82a43d
lucastribioli <luk.tribioli.nunes@gmail.com>
2025-05-30 13:33:29 -0300

    feat: implement Command pattern in Command.kt

---
+++ src/comportamentais/Command.kt
@@ -0,0 +1,51 @@
+package comportamentais
+
+fun main() {
+    val lightOnCommand = LightOnCommand(Light())
+    val lightOffCommand = LightOffCommand(Light())
+    val remoteControl = RemoteControl()
+    remoteControl.addCommand(lightOnCommand)
+    remoteControl.addCommand(lightOffCommand)
+    remoteControl.executeCommands()
+}
+
+interface Command {
+    fun action()
+}
+
+class Light{
+    fun turnOn() {
+        println("Light is turned on")
+    }
+
+    fun turnOff() {
+        println("Light is turned off")
+    }
+}
+
+class LightOnCommand(private val light: Light) : Command {
+    override fun action() {
+        light.turnOn()
+    }
+}
+
+class LightOffCommand(private val light: Light) : Command {
+    override fun action() {
+        light.turnOff()
+    }
+}
+
+class RemoteControl {
+    private val commands = mutableListOf<Command>()
+
+    fun addCommand(command: Command) {
+        commands.add(command)
+    }
+
+    fun executeCommands() {
+        for (command in commands) {
+            command.action()
+        }
+        commands.clear() // Clear commands after execution
+    }
+}
